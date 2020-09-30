afee1585884a5d5d1b9623215b1b75a1b7582963
Jimmy Lin <jimmylin0309@gmail.com>
2020-09-30 09:54:26 -0700

    add vote

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
+const Vote = require('./resolvers/Vote');
 
 const prisma = new PrismaClient();
 const pubsub = new PubSub();
@@ -12,8 +14,10 @@ const pubsub = new PubSub();
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 };
 
 const server = new GraphQLServer({
